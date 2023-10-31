033f1d777d2762212509c2cf86e2673ed9524fb4
91789 <91789@192.168.1.7>
2023-10-31 13:08:37 +0530

    my second commit

--- src/main/java/test1/AddToCart.java
+++ src/main/java/test1/AddToCart.java
@@ -2,7 +2,6 @@ package test1;
 
 
 
-import java.io.IOException;
 import java.time.Duration;
 import java.util.Iterator;
 import java.util.Set;
@@ -14,30 +13,46 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AddToCart {
 
-	public static void main(String[] args) throws IOException{
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
+	WebDriver driver;
+
+	@BeforeTest
+	public void setup()
+	{
+		//WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
 		driver.manage().window().maximize();
+	}
+	@Test
+	public void addcart()
+	{
 
+		//would launch the Chrome browser 
+		//by get method use the url
 		driver.get("https://www.flipkart.com/");
-		
 		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6000));
+
+		//findElement of class login
 		driver.findElement(By.className("_30XB9F")).click();
+
+		//findElement of class name search and enter
 		driver.findElement(By.name("q")).sendKeys("iphone" + Keys.ENTER);
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6000));
 
-
+		//findElement of class name product 
 		WebElement elink= driver.findElement(By.className("_396cs4"));
 		elink.click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6000));
 
-
-
+		//opens in another browser so use chindWindow
 		String mainwindow = driver.getWindowHandle();
 
 		Set<String> s1 = driver.getWindowHandles();
@@ -49,19 +64,25 @@ public class AddToCart {
 				driver.switchTo().window(ChildWindow);
 				System.out.println("chilwindow" + ChildWindow);
 
-
+				//add xpath of the product
 				driver.navigate().refresh(); 
 				WebElement addcart = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[2]/div[1]/ul[1]/li[1]/button[1]"));
 				WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
 
 				addcart = wait.until(ExpectedConditions.elementToBeClickable(addcart));
 				addcart.click();
+				driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6000));
 
 				System.out.println("Heading of child window is " + addcart.getText());
-				driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+				driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6000));
 			}
 		}    
 
 
+	}
+	@AfterTest
+	public void closeWindow()
+	{
+		driver.close();
 	}
 }
--- src/main/java/test1/FlipkartSearch.java
+++ src/main/java/test1/FlipkartSearch.java
@@ -1,34 +1,33 @@
 package test1;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class FlipkartSearch {
-
-	public static void main(String[] args) {
-		WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
+public class FlipkartSearch{
+	
+@Test
+public void FlipSearch() {
+	
+		
+		WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
         
         //with get method use the url
         driver.get("https://www.flipkart.com/");
-        driver.manage().deleteAllCookies();
-   
-        
+                
         //adding the claasName of search 
         driver.findElement(By.className("_30XB9F")).click();
    
         driver.findElement(By.name("q")).sendKeys("shoes"+Keys.ENTER);
   
         driver.findElement(By.className("_2r_T1I _396QI4")).click();
+        
+        driver.quit();
      
         
    }
-
 }
+
